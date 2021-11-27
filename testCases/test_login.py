0ded9dbc0c0b8215cf4e5c131ad09ac728919539
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-11-26 22:02:29 -0600

    Initial commit

---
+++ testCases/test_login.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from pageObjects.LoginPage import LoginPage
+
+class Test_001_Login
+    baseURL = "https://admin-demo.nopcommerce.com/"
+    username= "admin@yourstore.com"
+    password = "admin"
+
+    def test_homePageTitle(self,setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        act_title=self.driver.title
+        self.driver.close()
+        if act_title=="Your store. Login":
+            assert true
+            self.driver.close()
+        else:
+            self.driver.save_screenshot(".\\ScreenShots\\"+"test_homePageTitle.png")
+            self.driver.close()
+            assert False
+
+        def test_login(self,setup):
+            self.driver = setup
+            self.driver.get(self.baseURL)
+            self.lp=LoginPage(self.driver)
+            self.lp.setUserName(self.username)
+            self.lp.setPassword(self.password)
+            self.lp.clickLogin()
+            act_title=self.driver.title
+            self.driver.close()
+            if act_title=="Dashboard / nopCommerce administration"
+                assert True
+                self.driver.close()
+            else:
+                self.driver.save_screenshot(".\\ScreenShots\\"+"test_login.png")
+                self.driver.close()
+                assert False
